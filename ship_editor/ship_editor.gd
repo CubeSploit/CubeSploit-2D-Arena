33fbc9aad0fafead52f117f32899e8a4ee0367a6
Maveyyl <martialveyry@hotmail.fr>
2017-03-16 16:55:06 +0100

    Work on the grid of ship editor

--- ship_editor/ship_editor.gd
+++ ship_editor/ship_editor.gd
@@ -1,17 +1,36 @@
 extends Node2D
 
-onready var grid = get_node('grid')
+onready var camera = get_node('camera_2d')
+onready var grid_layer = get_node("grid/parallax_bg/parallax_layer")
+onready var grid_texture = get_node("grid/parallax_bg/parallax_layer/texture_frame")
+
 
 var wheel_pressed = false
 
+
+
 func _ready():
 	set_process_input(true)
+	
 	
 func _input(ev):
 	if( ev.type == InputEvent.MOUSE_BUTTON && ev.button_index == BUTTON_MIDDLE ):
 		wheel_pressed = ev.pressed
 	
 	if( ev.type == InputEvent.MOUSE_MOTION && wheel_pressed ):
-		pass
-#		grid.set_pos(grid.get_pos()+ev.relative_pos)
-#		get_viewport().set_canvas_transform(get_viewport().get_canvas_transform().translated(ev.relative_pos))
+		camera.set_pos(camera.get_pos()-ev.relative_pos)
+		
+	if( ev.type == InputEvent.MOUSE_BUTTON && ev.button_index == BUTTON_WHEEL_UP && ev.pressed ):
+		camera.set_zoom( camera.get_zoom()-Vector2(0.05,0.05) )
+
+#		grid_layer.set_mirroring(grid_layer.get_mirroring()/0.95)
+		grid_texture.set_size(OS.get_window_size())
+	if( ev.type == InputEvent.MOUSE_BUTTON && ev.button_index == BUTTON_WHEEL_DOWN && ev.pressed ):
+		camera.set_zoom( camera.get_zoom()+Vector2(0.05,0.05) )
+#		grid_layer.set_mirroring(grid_layer.get_mirroring()*0.95)
+#		grid_texture.set_size(grid_texture.get_size()/0.95)
+
+
+
+
+
